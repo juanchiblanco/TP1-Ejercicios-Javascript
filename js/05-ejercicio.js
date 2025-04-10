b44e848a1cf43023cd62ceb96f7626c6e63862c4
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-04-10 18:46:40 -0300

    agregue un programa de tres lineas donde suma los dos valores que el usuario ingresa

---
+++ js/05-ejercicio.js
@@ -0,0 +1,3 @@
+let numero1, numero2, resultado;
+numero1 = parseInt(prompt("Ingresa el primer número")), numero2 = parseInt(prompt("Ingresa el segundo número")); resultado = numero1 + numero2
+document.writeln("El resultado de la suma es: " + resultado)
