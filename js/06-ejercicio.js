b02e80b44e062860f3160ca8b35ba2ca73415819
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-04-10 19:09:32 -0300

    agregue un programa que devuelve el numero mas grande de dos valores ingresados

---
+++ js/06-ejercicio.js
@@ -0,0 +1,4 @@
+let numero1, numero2
+numero1 = parseInt(prompt("ingrese el primer numero"))
+numero2 = parseInt(prompt("ingrese el segundo numero"))
+if (numero1 > numero2){document.writeln(numero1 + " es el número mas grande")} else{document.writeln(numero2 + " es el número mas grande")}
