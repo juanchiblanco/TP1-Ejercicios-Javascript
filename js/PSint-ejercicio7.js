38d1876782c7731c7c35626b2f59c72060f00451
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-04-12 14:05:36 -0300

    agregue el ejercicio 7 de PSint

---
+++ js/PSint-ejercicio7.js
@@ -0,0 +1,3 @@
+let largo = parseInt(prompt("ingrese el largo del rectangulo")), ancho = parseInt(prompt("ingrese el ancho del rectangulo")), perimetro = 2 * (largo + ancho), area = largo * ancho
+document.writeln("El perímetro del rectángulo es: " + perimetro)
+document.writeln("<br>El area del rectángulo es: " + area)
