525c2770c9f2fc0b3b4d124607d577721d37ff1c
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-04-12 13:47:26 -0300

    agregue el ejercicio 4 de PSint

--- js/PSint-ejercicio3.js
+++ js/PSint-ejercicio3.js
@@ -1,2 +1,2 @@
-let numero1 = parseFloat(prompt("ingrese el primer numero")), resultado = numero1 * 10
+let numero1 = parseFloat(prompt("ingrese un numero")), resultado = numero1 * 10
 document.writeln("Su número multiplicado por 10 es: " + resultado)
---
+++ js/PSint-ejercicio4.js
@@ -0,0 +1,2 @@
+let numero1 = parseFloat(prompt("ingrese un numero"))
+if(numero1 % 2 === 0){document.writeln("Su numero es par")} else {document.writeln("Su numero es impar")}
