8f341e511d389707a6a0e8a9fa1ba65d0e0ac40c
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-04-12 13:11:22 -0300

    agregue un programa que indica si el numero ingresado es divisible en 2, 3, 5 o 7

---
+++ js/10-ejercicio.js
@@ -0,0 +1,5 @@
+let numero1 = parseInt(prompt("ingrese un numero"))
+if(numero1 % 2 === 0){document.writeln(numero1 + " es divisible en 2.")}
+if(numero1 % 3 === 0){document.writeln(numero1 + " es divisible en 3.")}
+if(numero1 % 5 === 0){document.writeln(numero1 + " es divisible en 5.")}
+if(numero1 % 7 === 0){document.writeln(numero1 + " es divisible en 7.")}
